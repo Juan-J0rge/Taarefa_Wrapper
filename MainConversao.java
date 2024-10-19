e6405b8417a645a40c5ed5123a0fdcb5fd9da926
Juan-J0rge <juancontatrabalho@gmail.com>
2024-10-19 00:57:51 -0300

    Tarefa Conversao

---
+++ MainConversao.java
@@ -0,0 +1,12 @@
+package TarefaConversao;
+
+public class MainConversao {
+    public static void main(String args[]) {
+        int numeroPrimitivo = 10;
+
+        Integer numeroWrapper = Integer.valueOf(numeroPrimitivo);
+
+        System.out.println("Valor Wrapper " + numeroWrapper);
+
+    }
+}
